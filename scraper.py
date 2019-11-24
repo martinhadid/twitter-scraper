b7b8d1a75dc45389179410165f28b64766588665
martinhadid <martinhadid@gmail.com>
2019-11-25 00:03:20 +0200

    Extract get_html, scroll for driver and move try and except to the main

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 #!usr/bin/python3
 from driver import Driver
 from bs4 import BeautifulSoup
-import time
 from csv import DictWriter
 import argparse
 import db_tools
@@ -14,38 +13,30 @@ CSV_HEADERS = ['tweet_id', 'date', 'username', 'tweets', 'hashtags', 'replies', 
 TWITTER_BASE_URL = 'http://www.twitter.com/'
 
 
-def scrape_tweets(driver):
-    try:
-        tweet_divs = driver.page_source
-        soup = BeautifulSoup(tweet_divs, 'html.parser')
-        all_tweets = soup.find_all('div', class_='content')
-        tweets = []
-        for tweet_html in all_tweets:
-            tweet = Tweet()
-            try:
-                tweet.enrich_tweet(tweet_html)
-                write_tweet_csv(tweet)
-                tweets.append(tweet)
-            except IndexError:
-                print('Not a tweet')
-        return tweets
-    except Exception as e:
-        print('Something went wrong!')
-        print(e)
-        driver.quit()
+def get_html(driver):
+    page_source = driver.get_page_source()
+    soup = BeautifulSoup(page_source, 'html.parser')
+    return soup
 
 
-def scrape_user(driver, username):
-    try:
-        user_divs = driver.page_source
-        soup = BeautifulSoup(user_divs, 'html.parser')
-        user = User(username)
-        user.enrich_user(soup)
-        return user
-    except IndexError as e:
-        print('Something went wrong!')
-        print(e)
-        driver.quit()
+def scrape_tweets(soup):
+    tweets = []
+    all_tweets = soup.find_all('div', class_='content')
+    for tweet_html in all_tweets:
+        tweet = Tweet()
+        try:
+            tweet.enrich_tweet(tweet_html)
+            write_tweet_csv(tweet)
+            tweets.append(tweet)
+        except IndexError:
+            print('Not a tweet')
+    return tweets
+
+
+def scrape_user(soup, username):
+    user = User(username)
+    user.enrich_user(soup)
+    return user
 
 
 def get_users(tweets):
@@ -76,14 +67,6 @@ def configure_search(word, start_date, end_date, language):
     return url
 
 
-def scroll(driver, url, max_time=2):
-    driver.get(url)
-    start_time = time.time()
-    while (time.time() - start_time) < max_time:
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        print(str(time.time() - start_time) + ' < ' + str(max_time))
-
-
 def write_csv_header():
     with open('twitterData.csv', 'w+') as csv_file:
         writer = DictWriter(csv_file, fieldnames=CSV_HEADERS)
@@ -107,33 +90,45 @@ def user_url(user):
     return TWITTER_BASE_URL + user
 
 
+def main_db():
+    db_tools.delete_db(DB_PATH)
+    db_tools.create_db_tables(DB_PATH)
+
+
 def main():
     args = get_argparser()
     url = configure_search(args['word'], args['start_date'], args['end_date'], args['language'])
-    driver = Driver().create()
-    scroll(driver, url, 5)
-    init_time = datetime.datetime.timestamp(datetime.datetime.now())
+
+    driver = Driver()
+    driver.scroll(url)
+
     write_csv_header()
-    db_tools.delete_db(DB_PATH)
-    db_tools.create_db_tables(DB_PATH)
-    tweets = scrape_tweets(driver)
-    usernames = get_users(tweets)
+
+    try:
+        tweets = scrape_tweets(get_html(driver))
+        usernames = get_users(tweets)
+    except Exception as e:
+        print('Something went wrong!')
+        print(e)
+        driver.quit()
+
     users = []
     print('The number of unique usernames gathered is:', len(usernames))
     i = 0
     for username in usernames[:1]:
         print('**********\nWere at user', i, 'out of', len(usernames), '\n******')
         url = user_url(username)
-        scroll(driver, url, .5)
-        users.append(scrape_user(driver, username))
-        tweets += scrape_tweets(driver)
+        driver.scroll(url, .5)
+        users.append(scrape_user(get_html(driver), username))
+        tweets += scrape_tweets(get_html(driver))
         i += 1
 
+    main_db()
+    init_time = datetime.datetime.timestamp(datetime.datetime.now())
     tweets_added = db_tools.write_tweets(tweets, init_time, DB_PATH)
     print('A total of', tweets_added, 'were added to DB.')
     users_added = db_tools.write_users(users, init_time, DB_PATH)
     print('A total of', users_added, 'users were added to DB.')
-    time.sleep(5)
     print('The tweets are ready!')
     driver.quit()
 
