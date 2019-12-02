258beb7726d7e44dcce19b214c193d2980c59fca
martinhadid <martinhadid@gmail.com>
2019-12-02 16:11:35 +0200

    Prevent type on saving to the db

--- tweet.py
+++ tweet.py
@@ -54,9 +54,9 @@ class Tweet:
     def _fetch_counters(self, scrap):
         """Sets all counters (retweets, likes and replies) from scrap"""
         tweet_counters = scrap.find_all('span', class_='ProfileTweet-actionCountForAria')
-        self.retweets = tweet_counters[config.tweet['retweets']].string.split(' ')[0]
-        self.likes = tweet_counters[config.tweet['likes']].string.split(' ')[0]
-        self.replies = tweet_counters[config.tweet['replies']].string.split(' ')[0]
+        self.retweets = int(tweet_counters[config.tweet['retweets']].string.split(' ')[0].replace(',', ''))
+        self.likes = int(tweet_counters[config.tweet['likes']].string.split(' ')[0].replace(',', ''))
+        self.replies = int(tweet_counters[config.tweet['replies']].string.split(' ')[0].replace(',', ''))
 
     def enrich_tweet(self, scrap):
         """Function that calls all tweet fetchers """
