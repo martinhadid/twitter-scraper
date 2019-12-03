b49158afc26cafb39fddbbf71119ddceeb87e4af
martinhadid <martinhadid@gmail.com>
2019-12-03 16:03:07 +0200

    Add info wrapped function

--- logger.py
+++ logger.py
@@ -3,6 +3,7 @@ import logging
 
 class Logger:
     def __init__(self):
+        """Initialize logger with file handler and formatter"""
         self.logger = logging.getLogger('TwitterLogger')
         self.fh = logging.FileHandler('scraper_logs.log')
         self.set_level()
@@ -10,13 +11,21 @@ class Logger:
         self.add_handler()
 
     def set_level(self):
+        """Set logger level to debug"""
         self.logger.setLevel(logging.DEBUG)
 
     def set_formatter(self):
+        """Set format for the logs"""
         self.fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 
     def add_handler(self):
+        """Add handler to write in file"""
         self.logger.addHandler(self.fh)
 
     def error(self, message):
+        """Wrapped function to log errors"""
         self.logger.error(message)
+
+    def info(self, message):
+        """Wrapped function to log info"""
+        self.logger.info(message)
