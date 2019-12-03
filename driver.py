c0d0d957a0bbba966df2f0d108ef961ec908c10e
martinhadid <martinhadid@gmail.com>
2019-12-03 16:02:58 +0200

    Improve docstrings

--- driver.py
+++ driver.py
@@ -9,21 +9,14 @@ class Driver:
         self.driver = webdriver.Chrome(os.path.join(os.path.dirname(__file__), config.driver))
 
     def scroll(self, url, max_time=2):
-        """
-        Scroll down the browser for the requested time
-        :param url: url to be requested
-        :param max_time: amount of time to be scrolling
-        """
+        """Scroll down the browser for the requested time"""
         self.driver.get(url)
         start_time = time.time()
         while (time.time() - start_time) < max_time:
             self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 
     def get_page_source(self):
-        """
-        Get page source from the browser
-        :return: page source to be scraped
-        """
+        """Get page source from the browser"""
         return self.driver.page_source
 
     def quit(self):
