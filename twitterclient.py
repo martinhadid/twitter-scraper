c59d0bbdf52a207183ccd7cd84128631bef0f5b2
martinhadid <martinhadid@gmail.com>
2019-12-11 17:07:37 +0200

    Create twitterclient to consume API

--- twitterclient.py
+++ twitterclient.py
@@ -1,45 +1,58 @@
 import tweepy
 from tweepy import OAuthHandler
 import config
-import api_translator
+from user import User
+import logger
+from textblob import TextBlob
+import re
 
-class TwitterClient(object):
-    '''Generic Twitter Class for sentiment analysis.'''
+"""global variable to log info and error to scraper_logs"""
+logger = logger.Logger()
+
+
+class TwitterClient:
 
     def __init__(self):
-        ''' Class constructor or initialization method.'''
-        # keys and tokens from the Twitter Dev Console
+        '''API connector'''
         self._consumer_key = config.API['consumer_key']
         self._consumer_secret = config.API['consumer_secret']
         self._access_token = config.API['access_token']
         self._access_token_secret = config.API['access_token_secret']
 
         try:
+            '''API Authentication'''
             self.auth = OAuthHandler(self._consumer_key, self._consumer_secret)
             self.auth.set_access_token(self._access_token, self._access_token_secret)
             self.api = tweepy.API(self.auth)
         except Exception:
-            print("Authentication Failed")
+            logger.error('Authentication Failed')
 
-    def get_user(self, user):
-        '''Main function to fetch tweets and parse them.'''
-        tweets = []
+    def get_users_missing_data(self, users):
+        '''Main function to fetch users and complete missing data.'''
         try:
-            user = self.api.get_user(id=user)
-            return user
+            for user in users:
+                user_data = self.api.get_user(id=user.username)
+                return [User(user.username,
+                             user_data.followers_count,
+                             user_data.friends_count,
+                             user_data.statuses_count)]
 
-        except tweepy.TweepError as e:
-            print("Error : " + str(e))
+        except tweepy.TweepError as err:
+            logger.error('Error : ' + str(err))
+
+    def clean_tweet(self, tweet):
+        ''' Utility function to clean tweet text'''
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w+:\ / \ / \S+)", " ", tweet).split())
+
+    def get_tweet_sentiment(self, tweet):
+        '''Classify sentiment of tweet'''
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
 
 
-def main():
-    print
-    api = TwitterClient()
-    # tweets = api.get_tweets(query='Bitcoin', count=1)
-    user = api.get_user('@fcbarcelona')
-    user = api_translator.from_api_user(user)
-    print('\n\n\n',user.total_tweets,user.username, user.following,user.followers)
-
-
-if __name__ == '__main__':
-    main()
